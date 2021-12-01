93a9fdaf1f4d95f3089e06e53a764335a05b20cd
ilusha2274 <ilya.Shamanin@yandex.ru>
2021-12-01 13:29:28 +0300

    Тепрь чат между 2 участниками можно сделать только через вкладку друзья

--- src/main/java/controller/NewMessageController.java
+++ src/main/java/controller/NewMessageController.java
@@ -3,24 +3,18 @@ package controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import repository.User;
 import repository.ChatRepository;
-import repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class NewMessageController {
 
     private final ChatRepository chatRepository;
-    private final UserRepository userRepository;
 
-    public NewMessageController(ChatRepository chatRepository, UserRepository userRepository) {
+    public NewMessageController(ChatRepository chatRepository) {
         this.chatRepository = chatRepository;
-        this.userRepository = userRepository;
     }
 
     @GetMapping("/newmessage")
@@ -33,50 +27,12 @@ public class NewMessageController {
     }
 
     @PostMapping("/newmessage")
-    public String newMessage(String email, String nameChat, @AuthenticationPrincipal User user, Model model) {
+    public String newMessage(String nameChat, @AuthenticationPrincipal User user, Model model) {
 
-        String result = "redirect:chat";
-        if (nameChat != null) {
-            addGroupChat(nameChat, user, model);
-        }
-
-        if (email != null) {
-            result = addPrivateChat(email, user, model);
-        }
-        return result;
-    }
-
-    private String addPrivateChat(String email, User user, Model model) {
-        User user2 = userRepository.findUserByEmail(email);
-        List<User> users = new ArrayList<>();
-        users.add(user);
-
-        if (user.getId() == user2.getId()){
-            model.addAttribute("activePage", "CHAT");
-            model.addAttribute("title", user.getName());
-            chatRepository.addChat(users, "saved");
-            return "redirect:chat";
-        }
-
-        users.add(user2);
-
-        if (user2 != null) {
-            model.addAttribute("activePage", "CHAT");
-            model.addAttribute("title", user.getName());
-            chatRepository.addChat(users, "private");
-            return "redirect:chat";
-        } else {
-            model.addAttribute("activePage", "NEWMESSAGES");
-            model.addAttribute("exception", "Пользователь не найден");
-            model.addAttribute("title", user.getName());
-            return "newmessage";
-        }
-    }
-
-    private String addGroupChat(String nameChat, User user, Model model) {
         chatRepository.addGroupChat(nameChat, "group", user);
         model.addAttribute("activePage", "CHAT");
         model.addAttribute("title", user.getName());
         return "redirect:chat";
     }
+
 }
--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import repository.ChatRepository;
 import repository.User;
 import exception.PasswordMismatchException;
 import exception.WrongEmailException;
@@ -15,16 +16,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Controller
 public class RegistrationController {
 
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
+    private final ChatRepository chatRepository;
 
-    public RegistrationController(UserRepository userRepository, AuthenticationManager authenticationManager) {
+    public RegistrationController(UserRepository userRepository, AuthenticationManager authenticationManager, ChatRepository chatRepository) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
+        this.chatRepository = chatRepository;
     }
 
     @GetMapping("/registration")
@@ -41,6 +45,9 @@ public class RegistrationController {
         try {
             if (!(userRepository.findEmailUser(email)) && userRepository.checkPassword(password, twoPassword)) {
                 User user = userRepository.addUser(newUser, twoPassword);
+                ArrayList<User> users = new ArrayList<>();
+                users.add(user);
+                chatRepository.addChat(users, "saved");
                 Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
